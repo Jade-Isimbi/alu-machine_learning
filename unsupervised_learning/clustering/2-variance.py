3b39a144b250dca31577b70ee304f0be4ea4093b
Jade Isimbi <jisimbi@bk.rw>
2026-02-23 09:44:37 +0200

    unsupervised learning

--- unsupervised_learning/clustering/2-variance.py
+++ unsupervised_learning/clustering/2-variance.py
@@ -10,20 +10,24 @@ import numpy as np
 
 def variance(X, C):
     """
-    calculates intra-cluster variance for a dataset
+    calculates total intra-cluster variance for a dataset
 
-    X: numpy.ndarray (n, d) containing the dataset that
-    will be used for K-means clustering
-        - n no. of data points
-        - d no. of dimensions for each data point
-    C: numpy.ndarray (k, d) containing the centroid
-        for each cluster
+    X: numpy.ndarray (n, d) containing the dataset
+    C: numpy.ndarray (k, d) containing the centroid means for each cluster
 
     return:
-        - var: total intra-cluster variance
+        - var: total variance, or None on failure
     """
-    var = np.sum((X - C[:, np.newaxis])**2, axis=-1)
-    mean = np.sqrt(var)
-    mini = np.min(mean, axis=0)
-    var = np.sum(mini ** 2)
-    return np.sum(var)
+    if type(X) is not np.ndarray or type(C) is not np.ndarray:
+        return None
+    if len(X.shape) != 2 or len(C.shape) != 2:
+        return None
+    n, d = X.shape
+    k, d_c = C.shape
+    if d != d_c or n == 0 or k == 0:
+        return None
+    # Squared distances: (k, n) - for each point, squared dist to each centroid
+    sq_dists = np.sum((X[np.newaxis, :, :] - C[:, np.newaxis, :]) ** 2, axis=-1)
+    # Min squared distance per point (nearest centroid), then sum
+    var = np.sum(np.min(sq_dists, axis=0))
+    return var
